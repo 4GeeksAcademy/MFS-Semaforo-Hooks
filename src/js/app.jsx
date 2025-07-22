636b661c0a643dd11b3268523c9c2e3834bd1aa1
MFriasSaavedra <martin.frias.saavedra@gmail.com>
2025-07-22 15:50:26 +0000

    traspaso codigo-ok

---
+++ src/js/app.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+const TrafficLight = () => {
+  const [light, setLight] = useState('red');
+  return (
+    <div className="traffic-light mt-5 d-flex">
+      <div className={`light red ${light === 'red' ? 'active' : ''}`} onClick={() => setLight('red')}/>
+      <div className={`light yellow ${light === 'yellow' ? 'active' : ''}`} onClick={() => setLight('yellow')}/>
+      <div className={`light green ${light === 'green' ? 'active' : ''}`} onClick={() => setLight('green')}/>
+    </div>
+  );
+};
+
+export default TrafficLight;
